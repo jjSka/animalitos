7327ceafecba61e4267795e44a9306e2460ef679
JJ <t_mac055@hotmail.com>
2019-11-07 16:46:04 +0100

    añadido carpeta proyecto

---
+++ PracticaAnimales/src/practicaanimales/Animal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaanimales;
+
+/**
+ *
+ * @author PC
+ */
+public class Animal {
+    
+    private String nombre;
+    private int edad;
+    private int peso;
+    private enum acuatico;
+    
+}
